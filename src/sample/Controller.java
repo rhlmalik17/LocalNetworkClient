ea0dc4f05a4be7e2c5a0d91b49ae114468b00345
Zeus <rhlmalik17@gmail.com>
2019-10-26 19:35:31 +0530

    BetaStage-01

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,38 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
 public class Controller {
+
+    @FXML
+    private ListView<Button> MenuList;
+
+    @FXML
+    public void initialize(){
+
+//        MenuList.setCellFactory(new Callback<ListView<HBox>, ListCell<HBox>>() {
+//            @Override
+//            public ListCell<HBox> call(ListView<HBox> hBoxListView) {
+//                ListCell<HBox> cell = new ListCell<HBox>(){
+//                    @Override
+//                    public void updateItem(HBox item,boolean empty)
+//                    {
+//                        super.updateItem(item,empty);
+//                        if(empty)
+//                        {
+//                            setText(null);
+//                        }else {
+//                            item.getChildren().get(0).setStyle("-fx-text-fill: white");
+//                        }
+//                    }
+//                };
+//                return cell;
+//            }
+//        });
+
+
+    }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,12 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        primaryStage.setTitle("Vii-Buu");
+        primaryStage.setScene(new Scene(root, 1500, 800));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
